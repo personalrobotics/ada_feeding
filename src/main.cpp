b7d15bd57400504215b2cb9eefa0baa6d87a1f6a
Ethan Gordon <ekgordon@cs.washington.edu>
2022-08-20 16:38:01 -0700

    [WIP] Builds linked to BehaviorTree

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 /**
  * Entry point to the ADA Feeding System
  **/
+#include <aikido/rviz.hpp>
 #include <behaviortree_cpp_v3/bt_factory.h>
+#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
 #include <feeding/nodes.hpp>
-#include <ros/ros.h>
-#include <ros/package.h>
 #include <libada/Ada.hpp>
+#include <ros/package.h>
+#include <ros/ros.h>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   // Initialize ROS node
   ros::init(argc, argv, "feeding");
-  std::shared_ptr<ros::NodeHandle> nh = std::make_shared<ros::NodeHandle>("~");
+  ros::NodeHandle nh("~");
   ros::AsyncSpinner spinner(2); // 2 threads
   spinner.start();
 
@@ -22,31 +23,35 @@ int main(int argc, char** argv)
 
   // Start Visualization
   ROS_INFO("Starting RViz Visualization...");
-  aikido::rviz::InteractiveMarkerViewer viewer(
-      nh.param("visualization/topicName", "dart_markers/feeding"),
-      nh.param("visualization/topicName", "dart_markers/feeding"),
-      robot.getWorld());
-  viewer->setAutoUpdate(true);
+  std::string vizTopic =
+      nh.param<std::string>("visualization/topicName", "dart_markers/feeding");
+  std::string vizBaseFrame =
+      nh.param<std::string>("visualization/basecFrameName", "map");
+  aikido::rviz::InteractiveMarkerViewer viewer(vizTopic, vizBaseFrame,
+                                               robot.getWorld());
+  viewer.setAutoUpdate(true);
 
   // Register Behavior Tree Nodes
   ROS_INFO("Initializing Behavior Tree...");
   BT::BehaviorTreeFactory factory;
-  feeding::registerNodes(factory, nh, robot);
+  // feeding::registerNodes(factory, nh, robot);
 
   // Create tree
-  std::string defaultTree = ros::package::getPath("ada_feeding") + "/trees/default.xml";
-  std::string treeFile = nh.param("treeFile", defaultTree);
+  std::string defaultTree =
+      ros::package::getPath("ada_feeding") + "/trees/default.xml";
+  std::string treeFile = nh.param<std::string>("treeFile", defaultTree);
   auto tree = factory.createTreeFromFile(treeFile);
   // This logger prints state changes on console
   BT::StdCoutLogger logger_cout(tree);
 
   // Run Tree until SUCCESS or FAILURE
   // Then repeat
-  while(ros::ok()) {
+  ros::Rate rate(100); // ROS Rate at 100Hz (10ms)
+  while (ros::ok()) {
     auto status = BT::NodeStatus::RUNNING;
-    while(status == BT::NodeStatus::RUNNING) {
+    while (status == BT::NodeStatus::RUNNING) {
       status = tree.tickRoot();
-      tree.sleep(std::chrono::milliseconds(10));
+      rate.sleep();
     }
   }
   return 0;
