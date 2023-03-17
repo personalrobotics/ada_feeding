9cfc37322487519c9df66078ff657ac6163a106d
Raida Karim <rk1997@cs.washington.edu>
2023-03-17 09:46:41 -0700

    add readme edits

--- scripts/README.md
+++ scripts/README.md
@@ -1,12 +1,7 @@
 ### How to Run Plate Locator Algorithm
-Open up 4 terminals in your computer (we assume you are using weebo that is connected to ADA robot in lab) and in each terminal run the following commands (1-4) after successfully building your workspace by respectively doing: `cd <catkin_ws>; catkin build; . devel/setup.bash`
+Open up some terminals in your computer (we assume you are using weebo that is connected to ADA robot in lab). Successfully build your workspace in each terminal by respectively doing: `cd <catkin_ws>; catkin build; . devel/setup.bash`. Run instrcutions 1-4 from [ada_feeding real robot demo](https://github.com/personalrobotics/ada_feeding/blob/main/README.md#demo-run-steps). Run the plate locator python script and behavior tree xml files with these commands respectively: `python ada_feeding/scripts/run_plate_locator.py` and `roslaunch ada_feeding feeding.launch treeFile:=plate.xml sim:=false use_forque:=false`.
 
-1. `roscore`
-2. `roslaunch ada_feeding rviz.launch`
-3. `python ada_feeding/scripts/run_plate_locator.py`
-4. `roslaunch ada_feeding feeding.launch treeFile:=plate.xml sim:=false use_forque:=false`
-
-Now, open up a fifth terminal and go to the directory of [feeding web app](https://github.com/personalrobotics/feeding_web_interface/tree/2022_revamp/feedingwebapp). From there run `npm start` which should start the app in your computer. If `npm` was not installed in your computer, run `npm install` before the `npm start` command.
+Now, open up another terminal and go to the directory of [feeding web app](https://github.com/personalrobotics/feeding_web_interface/tree/2022_revamp/feedingwebapp). From there run `npm start` which should start the app in your computer. If `npm` was not installed in your computer, run `npm install` before the `npm start` command. Also, please make sure to run `roslaunch rosbridge_server rosbridge_websocket.launch` to start the webserver so `ros` and app can communicate through topics.
 
 **Start App on Your Phone:** Connect your phone to the same wifi of your computer (in this case ADA_5G which is used by weebo). As the app is already running in weebo. Copy the url from the app tab's browser (something like `192.168.2.2:3000` which is weebo's IP address followed by port number) to browser in your phone. Then, the app should be running in your phone too. Go to the plate locator buttons by clicking on `Start Feeding` button from app's homepage.
 
--- scripts/run_plate_detector.py
+++ scripts/run_plate_detector.py
@@ -2,6 +2,7 @@
 # Import ROS libraries and messages
 import rospy
 from sensor_msgs.msg import Image
+import message_filters
 # Import OpenCV libraries and tools
 import cv2
 from cv_bridge import CvBridge
@@ -15,14 +16,20 @@ import tf
 # declare variables
 cv_image = None
 vector_array = None
+rot = np.empty((0, 5))
+trans = np.empty((0, 5))
+direction = np.empty((0, 5))
 detected = False
 bridge = CvBridge()
 
 # communicates with xml and js files
-def service_callback(req):
+def service_callback(self):
+    global cv_image
+    global rot
+    global trans
     # implement color detector
     # Converts images from BGR to HSV
-    if cv_image != None:
+    if cv_image is not None:
         hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         lower_blue = np.array([94, 80, 2])
         upper_blue = np.array([126, 255, 255])
@@ -47,6 +54,8 @@ def service_callback(req):
             # where w//2, h//2 are the required frame/image centeroid's XYcoordinates.
             centerX = w // 2
             centerY = h // 2
+            print('centerX', centerX)
+            print('centerY', centerY)
 
             # calculate moments of binary image
             m = cv2.moments(mask)
@@ -55,6 +64,8 @@ def service_callback(req):
             # calculate x,y coordinates of center
             cX = int(m["m10"] / m["m00"])
             cY = int(m["m01"] / m["m00"])
+            print('cX', cX)
+            print('cY', cY)
 
             #display the image
             #cv2.circle(cv_image, (cX, cY), 5, (255, 255, 255), -1)
@@ -67,16 +78,22 @@ def service_callback(req):
 
             deltaX = cX - centerX
             deltaY = cY - centerY
+            print('deltaX', deltaX)
+            print('deltaY', deltaY)
 
             if deltaX > 5 or deltaY > 5:
                 # get angle in randians
                 rad_angle = math.atan2(deltaX, deltaY)
                 vector_x = math.cos(rad_angle)
                 vector_y = math.sin(rad_angle)
+                print('vector_x', vector_x)
+                print('vector_y', vector_y)
 
                 # convert angle to vector
                 full_vector = [vector_x, vector_y, 0.0]
+                print('full vector', full_vector)
                 vector_array = np.asarray(full_vector)
+                print('vector_array', vector_array)
                 listener = tf.TransformListener()
                 while not rospy.is_shutdown():
                     try:
@@ -84,7 +101,9 @@ def service_callback(req):
                     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                         continue
                 camera_vector = np.reshape(vector_array, (3, 1))
+                print('camera_vector', camera_vector)
                 direction = np.dot(rot, camera_vector)
+                print('direction', direction)
 
         # return plate detect boolean and distance offset
         return PlateServiceResponse(detected, direction)
@@ -95,11 +114,15 @@ def service_callback(req):
 
 #  processes image
 def subscriber_callback(img_msg):
+    global cv_image
+    # Synchronize images
     # implement:
     # log some info about the image topic
-    rospy.loginfo(img_msg.header)
+    #rospy.loginfo(img_msg.header)
+    # print(img_msg.header)
     # Convert the ROS Image message to a CV2 Image
-    cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+    cv_image = bridge.imgmsg_to_cv2(img_msg)
+    cv2.imshow('cv', cv_image)
     # Show the converted image
     # show_image(cv_image)
 
@@ -108,15 +131,20 @@ def subscriber_callback(img_msg):
 def main():
     # We declare our node using init_node()
     rospy.init_node('starter', anonymous=True)
+    print("main entered")
 
     # communicates with camera topic
     # This declares that our node subscribes to the "/camera/rgb/image_raw" topic which is of type Image. 
     # When new ros image messages are received, subscriber_callback is invoked with the message as the first argument.
-    rospy.Subscriber("/camera/color/image_raw/image", Image, subscriber_callback)
+    ros_img = message_filters.Subscriber("/camera/color/image_raw", Image)
+    ts = message_filters.ApproximateTimeSynchronizer([ros_img], queue_size=10, slop=0.5)
+    ts.registerCallback(subscriber_callback)
+    print("after ts")
 
     # This declares a new service named 'alert_and_offset' with the PlateService service type. 
     # All requests are passed to service_callback function.
     s = rospy.Service('alert_and_offset', PlateService, service_callback)
+    print("after service")
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
