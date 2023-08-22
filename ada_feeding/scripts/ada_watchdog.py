23e7a522cf48478159a89b12ee76c8c1ba96dcc7
Amal Nanavati <amaln@uw.edu>
2023-08-21 17:24:28 -0700

    [WIP] Implemented e-stop node
    DONE:
    - Implemented a basic threshold-based check of whether the e-stop is clicked
    - Implemented an ACPI-event based detection of whether the e-stop was unplugged.
    - Tested it. The ACPI-event works. The threshold is too simplistic, because:
        - multiple frames exceed the threshold per click.
        - the threshold is exceeded upon unplug/replug.
    
    TODO:
    - Fix the issue in threshold implementation.
    - Create an abstract class for Watchdog condition that has a starting condition, a continuing condition, and contains its own error messages.
    - Modify the ADAWatchdog class to use that abstract class, and make FTCondition and EStopCondition subclass it.
    - Make the parameters ROS params, and pass them in with a config file.

--- ada_feeding/scripts/ada_watchdog.py
+++ ada_feeding/scripts/ada_watchdog.py
@@ -3,22 +3,26 @@
 This module contains a node, ADAWatchdog, which does the following:
     1. Monitors the state of the force-torque sensor, to ensure it is still
        publishing and its data is not zero-variance.
-    2. (TODO) Monitors the state of the physical e-stop button, to ensure it is
-       not pressed.
+    2. Monitors the state of the physical e-stop button, to ensure it is
+       plugged in (has received at least one click) and has not been pressed
+       since then (has not received a second click).
 This node publishes an output to the /ada_watchdog topic. Any node that moves
 the robot should subscribe to this topic and immediately stop if any of the
 watchdog conditions fail, or if the watchdog stops publishing.
 """
 
 # Standard imports
-from threading import Lock
+import socket
+from threading import Lock, Thread
 
 # Third-party imports
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 from geometry_msgs.msg import WrenchStamped
 import numpy as np
+import pyaudio
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 import rclpy
+from rclpy.clock import Clock
 from rclpy.duration import Duration
 from rclpy.node import Node
 from rclpy.time import Time
@@ -90,7 +94,7 @@ class FTSensorCondition:
                 ft_time,
             )
 
-    def check(self, now: Time) -> bool:
+    def ok(self, now: Time) -> bool:
         """
         Check if the force-torque sensor is still publishing and its data is not
         zero-variance.
@@ -111,6 +115,171 @@ class FTSensorCondition:
         zero-variance, False otherwise.
         """
         return np.all((now - self.last_unique_values_timestamp) <= self.timeout)
+
+
+class EStopCondition:
+    """
+    The EStopCondition class monitors the state of the physical e-stop button.
+
+    Specifically, it monitors the number of times the button has been pressed
+    (defined as a reading above/below a certain threshold) and fails if that
+    exceeds 1. This is because the first click is used to tell the watchdog
+    that the button is plugged in and working, and the watchdog will keep
+    failing until the first click is registered. After that, the watchdog will
+    pass until the button is clicked a second time, at which point it fails.
+    """
+
+    def __init__(
+        self,
+        clock: Clock,
+        initial_wait_secs: float,
+        rate: int,
+        chunk: int,
+        device: int,
+        min_threshold: int,
+        max_threshold: int,
+        acpi_event_name: str,
+    ) -> None:
+        """
+        Initialize the e-stop condition.
+
+        Parameters
+        ----------
+        clock: the ROS clock. Used to wait for `initial_wait_secs` at the beginning.
+        initial_wait_secs: the number of seconds to wait at the beginning. This is
+            because there is often initial noise in the audio readings.
+        rate: the rate (Hz) at which the audio readings are taken.
+        chunk: the number of audio readings to take at a time. If you are getting overflow
+            errors, try increasing this number.
+        device: the index of the audio device to use. This should usually be 0, for default
+            device input.
+        min_threshold (int16): the minimum threshold for the audio readings. If an audio reading
+            is below this threshold, the e-stop button is considered to be pressed.
+        max_threshold (int16): the maximum threshold for the audio readings. If an audio reading
+            is above this threshold, the e-stop button is considered to be pressed.
+        acpi_event_name: the name of the ACPI event that is published when the e-stop
+            button is (un)plugged. To find it, run `acpi_listen` in the terminal and unplug
+            the e-stop button. Exclude the suffix " plug"/" unplug".
+        """
+
+        # Initialize the parameters
+        self.clock = clock
+        self.initial_wait_duration = Duration(seconds=initial_wait_secs)
+        self.acpi_event_name = acpi_event_name
+        self.min_threshold = min_threshold
+        self.max_threshold = max_threshold
+
+        # Initialize the accumulators
+        self.start_time = None
+        self.num_clicks = 0
+        self.num_clicks_lock = Lock()
+        self.is_mic_unplugged = False
+        self.is_mic_unplugged_lock = Lock()
+
+        # Start listening for ACPI events on a separate thread
+        self.acpi_thread = Thread(target=self.__acpi_listener, daemon=True)
+        self.acpi_thread.start()
+
+        # Initialize the pyaudio object
+        self.audio = pyaudio.PyAudio()
+
+        # Initialize the stream, with a callback
+        self.stream = self.audio.open(
+            format=pyaudio.paInt16,
+            channels=1,  # The e-stop button is mono
+            rate=rate,
+            input=True,
+            frames_per_buffer=chunk,
+            input_device_index=device,
+            stream_callback=self.__audio_callback,
+        )
+
+    def __acpi_listener(self) -> None:
+        """
+        Listens for ACPI events. If the e-stop button is unplugged, sets the
+        `is_mic_unplugged` flag to True.
+        """
+        PLUG_KEYWORD = "plug"
+        UNPLUG_KEYWORD = "unplug"
+
+        # Open a socket to listen for ACPI events
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.connect("/var/run/acpid.socket")
+
+        # Listen for ACPI events
+        while rclpy.ok():
+            data_bytes = s.recv(4096)
+            data = data_bytes.decode("utf-8")
+            for event in data.split("\n"):
+                if self.acpi_event_name in event:
+                    keyword = event.split(" ")[-1]
+                    if UNPLUG_KEYWORD == keyword:
+                        with self.is_mic_unplugged_lock:
+                            self.is_mic_unplugged = True
+                    elif PLUG_KEYWORD == keyword:
+                        with self.is_mic_unplugged_lock:
+                            self.is_mic_unplugged = False
+
+    def __audio_callback(self, data, frame_count, time_info, status):
+        """
+        Callback function for the audio stream. This function is called whenever
+        the audio stream has new data. This function checks if the e-stop button
+        has been pressed, and if so, increments the number of clicks.
+
+        Parameters
+        ----------
+        data: the audio data, as a byte string
+        frame_count: the number of frames in the data
+        time_info: the time info
+        status: the status
+        """
+        # Skip the first few seconds of data, to avoid initial noise
+        if self.start_time is None:
+            self.start_time = self.clock.now()
+        if self.clock.now() - self.start_time < self.initial_wait_duration:
+            return (data, pyaudio.paContinue)
+
+        # Convert the data to a numpy array
+        data_arr = np.frombuffer(data, dtype=np.int16)
+
+        # Check if the e-stop button has been pressed
+        if np.any(
+            np.logical_or(data_arr < self.min_threshold, data_arr > self.max_threshold)
+        ):
+            with self.num_clicks_lock:
+                self.num_clicks += 1
+
+        # Return the data
+        return (data, pyaudio.paContinue)
+
+    def is_plugged_in(self) -> bool:
+        """
+        Returns True if the e-stop button is plugged in, False otherwise.
+
+        Specifically, this function returns True if the e-stop button has been
+        clicked at least once and no ACPI message has been received indicating
+        it has been unplugged, False otherwise.
+        """
+        with self.num_clicks_lock:
+            num_clicks = self.num_clicks
+        with self.is_mic_unplugged_lock:
+            is_mic_unplugged = self.is_mic_unplugged
+        return num_clicks > 0 and (not is_mic_unplugged)
+
+    def ok(self) -> bool:
+        """
+        Returns True if the e-stop button is plugged in (e.g., has been clicked once)
+        and hasn't been pressed since then (e.g., hasn't been clicked a second time).
+
+        Returns
+        -------
+        True if the e-stop button has been pressed exactly once, False otherwise.
+        """
+        with self.num_clicks_lock:
+            num_clicks = self.num_clicks
+        with self.is_mic_unplugged_lock:
+            is_mic_unplugged = self.is_mic_unplugged
+        return num_clicks == 1 and (not is_mic_unplugged)
 
 
 class ADAWatchdog(Node):
@@ -137,6 +306,18 @@ class ADAWatchdog(Node):
             DiagnosticArray,
             "~/watchdog",
             1,
+        )
+
+        # Create the e-stop conditions
+        self.e_stop_condition = EStopCondition(
+            self.get_clock(),
+            initial_wait_secs=2.0,
+            rate=48000,
+            chunk=4800,
+            device=0,
+            min_threshold=-10000,
+            max_threshold=10000,
+            acpi_event_name="jack/microphone MICROPHONE",
         )
 
         # Parameters for the force-torque conditions
@@ -228,6 +409,9 @@ class ADAWatchdog(Node):
         """
         Checks the watchdog conditions and publishes its output.
         """
+        self.get_logger().info(
+            f"E-Stop Condition first condition {self.e_stop_condition.is_plugged_in()} continuing condition {self.e_stop_condition.ok()} num clicks {self.e_stop_condition.num_clicks}"
+        )
         # Only publish if we've received the first force-torque sensor message
         recv_first_ft_msg = False
         with self.ft_sensor_condition_lock:
@@ -241,7 +425,7 @@ class ADAWatchdog(Node):
         # Return the output
         if recv_first_ft_msg:
             # Check the force-torque sensor conditions
-            ft_condition = self.ft_sensor_condition.check(now)
+            ft_condition = self.ft_sensor_condition.ok(now)
 
             # Generate the watchdog output
             if ft_condition:
