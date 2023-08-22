7bdc24358adeb7f0990271c162f798bb21ea6873
Amal Nanavati <amaln@uw.edu>
2023-08-22 11:49:38 -0700

    [WIP] Added WatchdogCondition abstract class, implemented FTSensorCondition

---
+++ ada_feeding/ada_feeding/watchdog/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains the conditions that are checked by the watchdog.
+"""
+
+from .watchdog_condition import WatchdogCondition
+from .ft_sensor_condition import FTSensorCondition
