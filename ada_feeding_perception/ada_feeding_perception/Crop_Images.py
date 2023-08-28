6cd8eebee8579e5d89fbaacac0a35e67b59e2673
Atharva Kashyap <atharva2@uw.edu>
2023-08-28 09:29:56 -0700

    added some necessary folders for NB

--- ada_feeding_perception/ada_feeding_perception/Crop_Images.py
+++ ada_feeding_perception/ada_feeding_perception/Crop_Images.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 
 
-def crop_img(imageToSave, rectangle_left_top=(297, 248), rectangle_right_bottom=(422, 332)):
+def crop_img(imageToSave, rectangle_left_top=(297, 248), rectangle_right_bottom=(422+3, 332)):
     cropped_img = np.copy(imageToSave)
     left_top_x, left_top_y = rectangle_left_top
     right_bottom_x, right_bottom_y = rectangle_right_bottom
@@ -21,8 +21,8 @@ def crop_img(imageToSave, rectangle_left_top=(297, 248), rectangle_right_bottom=
 
 if __name__ == "__main__":
     rosbags_abs_location = "/home/atharva2/atharvak_ws/src/rosbags/"
-    cropped_img_storage_abs_location = "/home/atharva2/atharvak_ws/src/cropped_images/"
-    non_cropped_img_storage_abs_location = "/home/atharva2/atharvak_ws/src/non_cropped_images/"
+    cropped_img_storage_abs_location = "/home/atharva2/atharvak_ws/src/with_real_labels/cropped_images/"
+    non_cropped_img_storage_abs_location = "/home/atharva2/atharvak_ws/src/with_real_labels/non_cropped_images/"
     master_labels = "../ada_feeding_perception/Master_with_labels_7-11-23.csv"
     depth_img_folder = "depth_img/"
 
@@ -41,15 +41,22 @@ if __name__ == "__main__":
     if not os.path.exists(cropped_img_storage_abs_location + depth_img_folder + "/no_food"):
         os.makedirs(cropped_img_storage_abs_location + depth_img_folder + "/no_food")
 
+    if not os.path.exists(cropped_img_storage_abs_location + depth_img_folder + "/hand"):
+        os.makedirs(cropped_img_storage_abs_location + depth_img_folder + "/hand")
+
     if not os.path.exists(non_cropped_img_storage_abs_location + depth_img_folder + "/food"):
         os.makedirs(non_cropped_img_storage_abs_location + depth_img_folder + "/food")
 
     if not os.path.exists(non_cropped_img_storage_abs_location + depth_img_folder + "/no_food"):
         os.makedirs(non_cropped_img_storage_abs_location + depth_img_folder + "/no_food")
 
+    if not os.path.exists(non_cropped_img_storage_abs_location + depth_img_folder + "/hand"):
+        os.makedirs(non_cropped_img_storage_abs_location + depth_img_folder + "/hand")
+
     # go through the list of rosbags present in the dir_list
     rosbags_dir_list = os.listdir(rosbags_abs_location)
 
+    count = 0
     for rosbag in rosbags_dir_list:
         with Reader(rosbags_abs_location + rosbag) as reader:
             for connection, timestamp, rawdata in reader.messages():
@@ -65,6 +72,8 @@ if __name__ == "__main__":
                                 df['timestamp_sec'] == timestamp_sec) & (df['timestamp_nanosec'] == timestamp_nsec)]
                     label = filtered_df['label'].iloc[0]
 
+                    # if label == 'hand':
+                    #     label = 'food'
                     storage_val = depth_img_folder + label + "/" + rosbag + "_" + str(timestamp_sec) + "_" + str(
                         timestamp_nsec) + ".png"
 
@@ -76,5 +85,7 @@ if __name__ == "__main__":
                     cropped_img = crop_img(imageToSave)
                     storageloc = cropped_img_storage_abs_location + storage_val
                     cv.imwrite(storageloc, cropped_img)
+                    count += 1
 
         print("done with: ", rosbag)
+    print("Count: ", count)
