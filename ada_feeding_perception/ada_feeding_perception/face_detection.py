a3c0f60ebcea53177c4857401e5539e0659aa2eb
Amal Nanavati <amaln@uw.edu>
2023-09-13 18:51:35 -0700

    Separated face detection into helper functions

--- ada_feeding_perception/ada_feeding_perception/face_detection.py
+++ ada_feeding_perception/ada_feeding_perception/face_detection.py
@@ -9,12 +9,12 @@ of the largest detected mouth with respect to camera_depth_optical_frame.
 import collections
 import os
 import threading
-from typing import Tuple
+from typing import Tuple, Union
 
 # Third-party imports
 import cv2
 from cv_bridge import CvBridge
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -292,7 +292,7 @@ class FaceDetectionNode(Node):
         with self.camera_info_lock:
             self.camera_matrix = msg.k
 
-    def depth_callback(self, msg: CompressedImage):
+    def depth_callback(self, msg: Image):
         """
         Callback function for depth images. If face_detection is on, this
         function publishes the 3d location of the mouth on the /face_detection
@@ -303,7 +303,7 @@ class FaceDetectionNode(Node):
         with self.depth_buffer_lock:
             self.depth_buffer.append(msg)
 
-    def camera_callback(self, msg: CompressedImage):
+    def camera_callback(self, msg: Union[CompressedImage, Image]):
         """
         Callback function for the camera feed. If face detection is on, this
         function will detect faces in the image and store the location of the
@@ -312,6 +312,163 @@ class FaceDetectionNode(Node):
         with self.latest_img_msg_lock:
             self.latest_img_msg = msg
 
+    def detect_largest_face(
+        self, img_msg: Union[CompressedImage, Image], publish_annotated_img: bool = True
+    ) -> Tuple[bool, Tuple[int, int], np.ndarray]:
+        """
+        Detect the largest face in an RGB image.
+
+        Parameters
+        ----------
+        img_msg: The RGB image to detect faces in.
+        publish_annotated_img: Whether to publish an annotated image with the
+            detected faces and mouth points.
+
+        Returns
+        -------
+        is_face_detected: Whether a face was detected in the image.
+        mouth_center: The (u,v) coordinates of the somion of the largest face
+            detected in the image.
+        mouth_points: A list of (u,v) coordinates of the facial landmarks of the mouth.
+        """
+
+        # pylint: disable=too-many-locals
+        # This function is not too complex, but it does have a lot of local variables.
+
+        # Decode the image
+        image_rgb = cv2.imdecode(
+            np.frombuffer(img_msg.data, np.uint8), cv2.IMREAD_COLOR
+        )
+        image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces using the haarcascade classifier on the grayscale image
+        # NOTE: This detector will launch multiple threads to detect faces in
+        # parallel.
+        faces = self.detector.detectMultiScale(image_gray)
+        is_face_detected = len(faces) > 0
+        img_mouth_center = (0, 0)
+        img_mouth_points = []
+
+        if is_face_detected:
+            # Detect landmarks (a 3d list) on image
+            # Relevant dimensions are 1: faces and 3: individual landmarks 0-68
+            _, landmarks = self.landmark_detector.fit(image_gray, faces)
+
+            # Add face markers to the image and find largest face
+            largest_face = [0, 0]  # [area (px^2), index]
+            for i, face in enumerate(faces):
+                (x, y, w, h) = face
+
+                # Update the largest face
+                if w * h > largest_face[0]:
+                    largest_face = [w * h, i]
+
+                # Annotate the image with the face. See below for the landmark indices:
+                # https://pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
+                if publish_annotated_img:
+                    cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (255, 255, 255), 2)
+                    for j in range(48, 68):
+                        landmark_x, landmark_y = landmarks[i][0][j]
+                        cv2.circle(
+                            image_rgb,
+                            (int(landmark_x), int(landmark_y)),
+                            1,
+                            (0, 255, 0),
+                            5,
+                        )
+
+            # Annotate the image with a red rectangle around largest face
+            (x, y, w, h) = faces[largest_face[1]]
+            cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+            # Find stomion (mouth center) in image
+            img_mouth_center = landmarks[largest_face[1]][0][66]
+            img_mouth_points = landmarks[largest_face[1]][0][48:68]
+
+        # Publish annotated image
+        if publish_annotated_img:
+            annotated_msg = cv2_image_to_ros_msg(image_rgb, compress=False)
+            self.publisher_image.publish(annotated_msg)
+
+        return is_face_detected, img_mouth_center, img_mouth_points
+
+    def get_mouth_depth(
+        self, rgb_msg: Union[CompressedImage, Image], face_points: np.ndarray
+    ) -> float:
+        """
+        Get the depth of the mouth of the largest detected face in the image.
+
+        Parameters
+        ----------
+        rgb_msg: The RGB image that the face was detected in.
+        face_points: A list of (u,v) coordinates of the facial landmarks whose depth
+            should be averaged to get the predicted depth of the mouth.
+
+        Returns
+        -------
+        depth_mm: The depth of the mouth in mm.
+        """
+        # Find depth image closest in time to RGB image that face was in
+        with self.depth_buffer_lock:
+            min_time_diff = float("inf")
+            closest_depth_msg = None
+            for depth_msg in self.depth_buffer:
+                depth_time = depth_msg.header.stamp.sec + (
+                    depth_msg.header.stamp.nanosec / (10**9)
+                )
+                img_time = rgb_msg.header.stamp.sec + (
+                    rgb_msg.header.stamp.nanosec / (10**9)
+                )
+                time_diff = abs(depth_time - img_time)
+                if time_diff < min_time_diff:
+                    min_time_diff = time_diff
+                    closest_depth_msg = depth_msg
+        image_depth = self.bridge.imgmsg_to_cv2(
+            closest_depth_msg,
+            desired_encoding="passthrough",
+        )
+
+        # Retrieve the depth value averaged over all mouth coordinates
+        depth_sum = 0
+        for point in face_points:
+            depth_sum += image_depth[int(point[1])][int(point[0])]
+        depth_mm = depth_sum / float(len(face_points))
+
+        return depth_mm
+
+    def get_stomion_point(self, u: int, v: int, depth_mm: float) -> Point:
+        """
+        Get the 3d location of the mouth. This function assumes the color
+        and depth images have the same frame, and returns the point in
+        that frame.
+
+        Parameters
+        ----------
+        u: The u coordinate of the mouth in the RGB image.
+        v: The v coordinate of the mouth in the RGB image.
+        depth_mm: The depth of the mouth in mm.
+
+        Returns
+        -------
+        mouth_point: The 3d location of the mouth in the camera frame.
+        """
+        # Get the camera matrix
+        with self.camera_info_lock:
+            camera_matrix = self.camera_matrix
+
+        # Compute the point. See https://www.youtube.com/watch?v=qByYk6JggQU&t=443s
+        # for the derivation of the formulae.
+        mouth_point = Point()
+        mouth_point.x = (float(depth_mm) * (float(u) - camera_matrix[2])) / (
+            1000.0 * camera_matrix[0]
+        )
+        mouth_point.y = (float(depth_mm) * (float(v) - camera_matrix[5])) / (
+            1000.0 * camera_matrix[4]
+        )
+        mouth_point.z = float(depth_mm) / 1000.0
+
+        return mouth_point
+
     def run(self) -> None:
         """
         Run face detection at the specified rate. Specifically, this function
@@ -319,11 +476,6 @@ class FaceDetectionNode(Node):
         to it in time, runs face detection on the RGB image, and then determines
         the depth of the detected face.
         """
-
-        # pylint: disable=too-many-locals
-        # TODO: Split the RGB part of face detection and the depth part of face
-        # detection into separate functions.
-
         rate = self.create_rate(self.rate_hz)
         while rclpy.ok():
             # Loop at the specified rate
@@ -342,93 +494,26 @@ class FaceDetectionNode(Node):
             with self.latest_img_msg_lock:
                 rgb_msg = self.latest_img_msg
 
-            # Decode the image
-            image_rgb = cv2.imdecode(
-                np.frombuffer(rgb_msg.data, np.uint8), cv2.IMREAD_COLOR
-            )
-            image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
-
-            # Detect faces using the haarcascade classifier on the grayscale image
-            faces = self.detector.detectMultiScale(image_gray)
-            is_face_detected = len(faces) > 0
+            # Detect the largest face in the RGB image
+            (
+                is_face_detected,
+                img_mouth_center,
+                img_mouth_points,
+            ) = self.detect_largest_face(rgb_msg)
 
             if is_face_detected:
-                # Detect landmarks (a 3d list) on image
-                # Relevant dimensions are 1: faces and 3: individual landmarks 0-68
-                _, landmarks = self.landmark_detector.fit(image_gray, faces)
+                # Get the depth of the mouth
+                depth_mm = self.get_mouth_depth(rgb_msg, img_mouth_points)
 
-                # Add face markers to the image and find largest face
-                largest_face = [0, 0]  # [area (px^2), index]
-                for i, face in enumerate(faces):
-                    (x, y, w, h) = face
-                    # Annotate the image with the face. See below for the landmark indices:
-                    # https://pyimagesearch.com/2017/04/03/facial-landmarks-dlib-opencv-python/
-                    cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (255, 255, 255), 2)
-                    for j in range(48, 68):
-                        x, y = landmarks[i][0][j]
-                        cv2.circle(image_rgb, (int(x), int(y)), 1, (0, 255, 0), 5)
-
-                    # Update the largest face
-                    if w * h > largest_face[0]:
-                        largest_face = [w * h, i]
-
-                # Annotate the image with a red rectangle around largest face
-                (x, y, w, h) = faces[largest_face[1]]
-                cv2.rectangle(image_rgb, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-                # Find stomion (mouth center) in image
-                img_mouth_center = landmarks[largest_face[1]][0][66]
-                u, v = int(round(img_mouth_center[0])), int(round(img_mouth_center[1]))
-                img_mouth_points = landmarks[largest_face[1]][0][48:68]
-
-                # Find depth image closest in time to saved color image
-                time_diffs = []
-                with self.depth_buffer_lock:
-                    for i, depth_msg in enumerate(self.depth_buffer):
-                        depth_time = depth_msg.header.stamp.sec + (
-                            depth_msg.header.stamp.nanosec / (10**9)
-                        )
-                        img_time = rgb_msg.header.stamp.sec + (
-                            rgb_msg.header.stamp.nanosec / (10**9)
-                        )
-                        time_diffs.append(abs(depth_time - img_time))
-                    closest_depth_msg = self.depth_buffer[np.argmin(time_diffs)]
-                image_depth = self.bridge.imgmsg_to_cv2(
-                    closest_depth_msg,
-                    desired_encoding="passthrough",
+                # Get the 3d location of the mouth
+                face_detection_msg.detected_mouth_center.header = rgb_msg.header
+                face_detection_msg.detected_mouth_center.point = self.get_stomion_point(
+                    img_mouth_center[0], img_mouth_center[1], depth_mm
                 )
-
-                # Retrieve the depth value averaged over all mouth coordinates
-                depth_sum = 0
-                for point in img_mouth_points:
-                    depth_sum += image_depth[int(point[1])][int(point[0])]
-                depth = depth_sum / float(len(img_mouth_points))
-
-                # Get the camera matrix
-                with self.camera_info_lock:
-                    camera_matrix = self.camera_matrix
-
-                # Create target 3d point, with mm measurements converted to m
-                # Equations and explanation can be found at https://youtu.be/qByYk6JggQU
-                mouth_location = PointStamped()
-                mouth_location.header = rgb_msg.header
-                mouth_location.point.x = (
-                    float(depth) * (float(u) - camera_matrix[2])
-                ) / (1000.0 * camera_matrix[0])
-                mouth_location.point.y = (
-                    float(depth) * (float(v) - camera_matrix[5])
-                ) / (1000.0 * camera_matrix[4])
-                mouth_location.point.z = float(depth) / 1000.0
-
-                face_detection_msg.detected_mouth_center = mouth_location
 
             # Publish 3d point
             face_detection_msg.is_face_detected = is_face_detected
             self.publisher_results.publish(face_detection_msg)
-
-            # Publish annotated image with face and mouth landmarks
-            annotated_msg = cv2_image_to_ros_msg(image_rgb, compress=False)
-            self.publisher_image.publish(annotated_msg)
 
 
 def main(args=None):
