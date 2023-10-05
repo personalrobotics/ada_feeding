914256d587c035924b07f02722dd5b1f8af1e55f
Amal Nanavati <amaln@uw.edu>
2023-10-05 00:06:34 -0700

    Base keyboard teleop node works

--- ada_feeding/scripts/ada_keyboard_teleop.py
+++ ada_feeding/scripts/ada_keyboard_teleop.py
@@ -12,6 +12,7 @@ import sys
 
 # Third-party imports
 from geometry_msgs.msg import TwistStamped
+import rclpy
 
 # Features to consider adding: cartesian angular, joint control, modifying linear/angular speed.
 msg = """
@@ -29,7 +30,7 @@ Joint control (UNIMPLEMENTED):
 CTRL-C to quit
 """
 
-def getKey():
+def getKey(settings):
     """
     Read a key from stdin without writing it to terminal.
     """
@@ -44,25 +45,65 @@ def getKey():
     return key
 
 cartesian_control_bindings = {
-    'w': (1, 0, 0), # forward
-    's': (-1, 0, 0), # backwards
-    'a': (0, 1, 0), # left
-    'd': (0, -1, 0), # right
-    'q': (0, 0, 1), # up
-    'e': (0, 0, -1), # down
+    'w': ( 1.0,  0.0,  0.0), # forward
+    's': (-1.0,  0.0,  0.0), # backwards
+    'a': ( 0.0,  1.0,  0.0), # left
+    'd': ( 0.0, -1.0,  0.0), # right
+    'q': ( 0.0,  0.0,  1.0), # up
+    'e': ( 0.0,  0.0, -1.0), # down
 }
 
-if __name__=="__main__":
+def main(args=None):
+    """
+    Launch the ROS node and spin.
+    """
     settings = termios.tcgetattr(sys.stdin)
+
+    # Initialize the ROS context
+    rclpy.init(args=args)
+    node = rclpy.create_node('ada_keyboard_teleop')
+    pub = node.create_publisher(TwistStamped, '/servo_node/delta_twist_cmds', 1)
+
+    # Create the message
+    twist_msg = TwistStamped()
+    twist_msg.header.frame_id = "j2n6s200_link_base"
+    
+    prev_key = ''
+
     try:
         while(1):
-            key = getKey()
-            print(repr(key))
+            rclpy.spin_once(node, timeout_sec=0)
+            key = getKey(settings)
 
-            # Ctrl+C interrupt
-            if (key == '\x03'):
-                break
+            if key in cartesian_control_bindings.keys():
+                # Due to keyboard delay before repeat, when the user holds down a
+                # key we will read it as the key, followed by some number of empty
+                # readings, followed by the key consecutively. To account for this,
+                # we require two consecutive readings of the same key before
+                # publishing the velcoity commands.
+                if prev_key == key:
+                    x, y, z = cartesian_control_bindings[key]
+                    twist_msg.twist.linear.x = x
+                    twist_msg.twist.linear.y = y
+                    twist_msg.twist.linear.z = z
+            else:
+                twist_msg.twist.linear.x = 0.0
+                twist_msg.twist.linear.y = 0.0
+                twist_msg.twist.linear.z = 0.0
+
+                # Ctrl+C Interrupt
+                if (key == '\x03'):
+                    break
+
+            # Publish the message
+            twist_msg.header.stamp = node.get_clock().now().to_msg()
+            pub.publish(twist_msg)
+
+            prev_key = key
     except Exception as e:
         print(repr(e))
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+if __name__=="__main__":
+    main()
