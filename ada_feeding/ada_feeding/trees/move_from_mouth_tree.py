c7b07025d029b93d7f225fe6ca5dc7711555389e
Amal Nanavati <amaln@uw.edu>
2023-11-21 18:46:03 -0800

    Finalize MoveFromMouth

--- ada_feeding/ada_feeding/trees/move_from_mouth_tree.py
+++ ada_feeding/ada_feeding/trees/move_from_mouth_tree.py
@@ -21,17 +21,19 @@ from ada_feeding.behaviors.moveit2 import (
     MoveIt2Plan,
     MoveIt2Execute,
     MoveIt2JointConstraint,
-    MoveIt2PositionConstraint,
     MoveIt2OrientationConstraint,
 )
+from ada_feeding.behaviors.ros import CreatePoseStamped
 from ada_feeding.helpers import BlackboardKey
-from ada_feeding.idioms import pre_moveto_config, scoped_behavior
+from ada_feeding.idioms import pre_moveto_config, scoped_behavior, servo_until_pose
 from ada_feeding.idioms.bite_transfer import (
     get_add_in_front_of_wheelchair_wall_behavior,
     get_toggle_collision_object_behavior,
     get_remove_in_front_of_wheelchair_wall_behavior,
 )
 from ada_feeding.trees import MoveToTree
+from .start_servo_tree import StartServoTree
+from .stop_servo_tree import StopServoTree
 
 
 class MoveFromMouthTree(MoveToTree):
@@ -50,14 +52,8 @@ class MoveFromMouthTree(MoveToTree):
         staging_configuration_position: Tuple[float, float, float],
         staging_configuration_quat_xyzw: Tuple[float, float, float, float],
         end_configuration: Optional[List[float]] = None,
-        staging_configuration_tolerance: float = 0.001,
+        staging_configuration_tolerance_position: float = 0.005,
         end_configuration_tolerance: float = 0.001,
-        orientation_constraint_to_staging_configuration_quaternion: Optional[
-            List[float]
-        ] = None,
-        orientation_constraint_to_staging_configuration_tolerances: Optional[
-            List[float]
-        ] = None,
         orientation_constraint_to_end_configuration_quaternion: Optional[
             List[float]
         ] = None,
@@ -65,15 +61,13 @@ class MoveFromMouthTree(MoveToTree):
             List[float]
         ] = None,
         planner_id: str = "RRTstarkConfigDefault",
-        allowed_planning_time_to_staging_configuration: float = 0.5,
         allowed_planning_time_to_end_configuration: float = 0.5,
-        max_velocity_scaling_factor_to_staging_configuration: float = 0.1,
         max_velocity_scaling_factor_to_end_configuration: float = 0.1,
-        cartesian_jump_threshold_to_staging_configuration: float = 0.0,
-        cartesian_max_step_to_staging_configuration: float = 0.0025,
         wheelchair_collision_object_id: str = "wheelchair_collision",
-        force_threshold: float = 4.0,
-        torque_threshold: float = 4.0,
+        force_threshold_to_staging_configuration: float = 1.0,
+        torque_threshold_to_staging_configuration: float = 1.0,
+        force_threshold_to_end_configuration: float = 4.0,
+        torque_threshold_to_end_configuration: float = 4.0,
     ):
         """
         Initializes tree-specific parameters.
@@ -86,43 +80,28 @@ class MoveFromMouthTree(MoveToTree):
             configuration.
         end_configuration: The joint positions to move the robot arm to after
             going to the staging configuration.
-        staging_configuration_tolerance: The tolerance for the joint positions.
+        staging_configuration_tolerance_position: The tolerance for the joint positions.
         end_configuration_tolerance: The tolerance for the joint positions.
-        orientation_constraint_to_staging_configuration_quaternion: The
-            quaternion for the orientation constraint to the staging
-            configuration.
-        orientation_constraint_to_staging_configuration_tolerances: The
-            tolerances for the orientation constraint to the staging
-            configuration.
         orientation_constraint_to_end_configuration_quaternion: The
             quaternion for the orientation constraint to the end configuration.
         orientation_constraint_to_end_configuration_tolerances: The
             tolerances for the orientation constraint to the end configuration.
         planner_id: The planner ID to use for the MoveIt2 motion planning.
-        allowed_planning_time_to_staging_configuration: The allowed planning
-            time for the MoveIt2 motion planner to move to the staging config.
         allowed_planning_time_to_end_configuration: The allowed planning
             time for the MoveIt2 motion planner to move to the end config.
-        max_velocity_scaling_factor_to_staging_configuration: The maximum
-            velocity scaling factor for the MoveIt2 motion planner to move to
-            the staging config.
         max_velocity_scaling_factor_to_end_configuration: The maximum
             velocity scaling factor for the MoveIt2 motion planner to move to
             the end config.
-        cartesian_jump_threshold_to_staging_configuration: The cartesian
-            jump threshold for the MoveIt2 motion planner to move to the
-            staging config.
-        cartesian_max_step_to_staging_configuration: The cartesian
-            max step for the MoveIt2 motion planner to move to the
-            staging config.
         wheelchair_collision_object_id: The ID of the wheelchair collision object
             in the MoveIt2 planning scene.
-        force_threshold: The force threshold (N) for the ForceGateController.
-            For now, the same threshold is used to move to the staging location
-            and to the mouth.
-        torque_threshold: The torque threshold (N*m) for the ForceGateController.
-            For now, the same threshold is used to move to the staging location
-            and to the mouth.
+        force_threshold_to_staging_configuration: The force threshold for the
+            motion to the staging config.
+        torque_threshold_to_staging_configuration: The torque threshold for the
+            motion to the staging config.
+        force_threshold_to_end_configuration: The force threshold for the
+            motion to the end config.
+        torque_threshold_to_end_configuration: The torque threshold for the
+            motion to the end config.
         """
         # Initialize MoveToTree
         super().__init__(node)
@@ -133,14 +112,8 @@ class MoveFromMouthTree(MoveToTree):
         self.end_configuration = end_configuration
         if self.end_configuration is not None:
             assert len(self.end_configuration) == 6, "Must provide 6 joint positions"
-        self.staging_configuration_tolerance = staging_configuration_tolerance
+        self.staging_configuration_tolerance_position = staging_configuration_tolerance_position
         self.end_configuration_tolerance = end_configuration_tolerance
-        self.orientation_constraint_to_staging_configuration_quaternion = (
-            orientation_constraint_to_staging_configuration_quaternion
-        )
-        self.orientation_constraint_to_staging_configuration_tolerances = (
-            orientation_constraint_to_staging_configuration_tolerances
-        )
         self.orientation_constraint_to_end_configuration_quaternion = (
             orientation_constraint_to_end_configuration_quaternion
         )
@@ -148,27 +121,21 @@ class MoveFromMouthTree(MoveToTree):
             orientation_constraint_to_end_configuration_tolerances
         )
         self.planner_id = planner_id
-        self.allowed_planning_time_to_staging_configuration = (
-            allowed_planning_time_to_staging_configuration
-        )
         self.allowed_planning_time_to_end_configuration = (
             allowed_planning_time_to_end_configuration
-        )
-        self.max_velocity_scaling_factor_to_staging_configuration = (
-            max_velocity_scaling_factor_to_staging_configuration
         )
         self.max_velocity_scaling_factor_to_end_configuration = (
             max_velocity_scaling_factor_to_end_configuration
         )
-        self.cartesian_jump_threshold_to_staging_configuration = (
-            cartesian_jump_threshold_to_staging_configuration
-        )
-        self.cartesian_max_step_to_staging_configuration = (
-            cartesian_max_step_to_staging_configuration
-        )
         self.wheelchair_collision_object_id = wheelchair_collision_object_id
-        self.force_threshold = force_threshold
-        self.torque_threshold = torque_threshold
+        self.force_threshold_to_staging_configuration = (
+            force_threshold_to_staging_configuration
+        )
+        self.torque_threshold_to_staging_configuration = (
+            torque_threshold_to_staging_configuration
+        )
+        self.force_threshold_to_end_configuration = force_threshold_to_end_configuration
+        self.torque_threshold_to_end_configuration = torque_threshold_to_end_configuration
 
     @override
     def create_tree(
@@ -180,30 +147,8 @@ class MoveFromMouthTree(MoveToTree):
         ### Define tree logic
 
         in_front_of_wheelchair_wall_id = "in_front_of_wheelchair_wall"
+        base_link = "j2n6s200_link_base"
 
-        # The tree may or may not have orientation path constraints active
-        if self.orientation_constraint_to_staging_configuration_quaternion is None:
-            # pylint: disable=abstract-class-instantiated
-            # This is fine, not sure why pylint doesn't like it
-            staging_path_constraints = py_trees.behaviours.Success()
-        else:
-            # Orientation path constraint to keep the fork straight
-            staging_path_constraints = MoveIt2OrientationConstraint(
-                name="KeepForkStraightPathConstraint",
-                ns=name,
-                inputs={
-                    "quat_xyzw": (
-                        self.orientation_constraint_to_staging_configuration_quaternion
-                    ),
-                    "tolerance": (
-                        self.orientation_constraint_to_staging_configuration_tolerances
-                    ),
-                    "parameterization": 1,  # Rotation vector
-                },
-                outputs={
-                    "constraints": BlackboardKey("path_constraints"),
-                },
-            )
         if self.orientation_constraint_to_end_configuration_quaternion is None:
             # pylint: disable=abstract-class-instantiated
             # This is fine, not sure why pylint doesn't like it
@@ -232,88 +177,76 @@ class MoveFromMouthTree(MoveToTree):
             name=name,
             memory=True,
             children=[
-                # Retare the F/T sensor and set the F/T Thresholds
-                pre_moveto_config(
-                    name=name + "PreMoveToConfig",
-                    toggle_watchdog_listener=False,
-                    f_mag=self.force_threshold,
-                    t_mag=self.torque_threshold,
-                ),
                 # Allow collisions between the robot and the expanded wheelchair
                 # collision object
                 scoped_behavior(
                     name=name + " AllowWheelchairCollisionScope",
-                    pre_behavior=get_toggle_collision_object_behavior(
-                        name + "AllowWheelchairCollisionScopePre",
-                        [self.wheelchair_collision_object_id],
-                        True,
+                    pre_behavior=py_trees.composites.Sequence(
+                        name=name,
+                        memory=True,
+                        children=[
+                            get_toggle_collision_object_behavior(
+                                name + "AllowWheelchairCollisionScopePre",
+                                [self.wheelchair_collision_object_id],
+                                True,
+                            ),
+                            StartServoTree(self._node)
+                            .create_tree(name=name + "StartServoScopePre")
+                            .root,
+                        ],
+                    ),
+                    post_behavior=py_trees.composites.Sequence(
+                        name=name,
+                        memory=True,
+                        children=[
+                            StopServoTree(self._node)
+                            .create_tree(name=name + "StopServoScopePost")
+                            .root,
+                            get_toggle_collision_object_behavior(
+                                name + "DisallowWheelchairCollisionScopePost",
+                                [self.wheelchair_collision_object_id],
+                                False,
+                            ),
+                            pre_moveto_config(
+                                name=name + "PreMoveToConfigScopePost",
+                                re_tare=False,
+                                f_mag=1.0,
+                                param_service_name="~/set_servo_controller_parameters",
+                            ),
+                        ],
                     ),
                     workers=[
-                        # Goal configuration: target position
-                        MoveIt2PositionConstraint(
-                            name="MoveToStagingPosePositionGoalConstraint",
+                        # Retare the F/T sensor and set the F/T Thresholds
+                        pre_moveto_config(
+                            name=name + "PreMoveToConfigToStaging",
+                            toggle_watchdog_listener=False,
+                            f_mag=self.force_threshold_to_staging_configuration,
+                            t_mag=self.torque_threshold_to_staging_configuration,
+                        ),
+                        # Create the goal pose
+                        CreatePoseStamped(
+                            name=name + "CreateStagingPose",
                             ns=name,
                             inputs={
                                 "position": self.staging_configuration_position,
-                                "tolerance": self.staging_configuration_tolerance,
+                                "quaternion": self.staging_configuration_quat_xyzw,
+                                "frame_id": base_link,
                             },
                             outputs={
-                                "constraints": BlackboardKey("goal_constraints"),
+                                "pose_stamped": BlackboardKey("goal_pose"),
                             },
                         ),
-                        # Goal configuration: target orientation
-                        MoveIt2OrientationConstraint(
-                            name="MoveToStagingPoseOrientationGoalConstraint",
+                        # Servo until the goal pose
+                        servo_until_pose(
+                            name=name + " MoveToStagingPose",
                             ns=name,
-                            inputs={
-                                "quat_xyzw": self.staging_configuration_quat_xyzw,
-                                "tolerance": (0.6, 0.5, 0.5),
-                                "parameterization": 1,  # Rotation vector
-                                "constraints": BlackboardKey("goal_constraints"),
-                            },
-                            outputs={
-                                "constraints": BlackboardKey("goal_constraints"),
-                            },
-                        ),
-                        staging_path_constraints,
-                        # Plan
-                        MoveIt2Plan(
-                            name="MoveToStagingPosePlan",
-                            ns=name,
-                            inputs={
-                                "goal_constraints": BlackboardKey("goal_constraints"),
-                                "path_constraints": BlackboardKey("path_constraints"),
-                                "planner_id": self.planner_id,
-                                "allowed_planning_time": (
-                                    self.allowed_planning_time_to_staging_configuration
-                                ),
-                                "max_velocity_scale": (
-                                    self.max_velocity_scaling_factor_to_staging_configuration
-                                ),
-                                "cartesian": True,
-                                "cartesian_jump_threshold": (
-                                    self.cartesian_jump_threshold_to_staging_configuration
-                                ),
-                                "cartesian_fraction_threshold": 0.60,
-                                "cartesian_max_step": (
-                                    self.cartesian_max_step_to_staging_configuration
-                                ),
-                            },
-                            outputs={"trajectory": BlackboardKey("trajectory")},
-                        ),
-                        # Execute
-                        MoveIt2Execute(
-                            name="MoveToStagingPoseExecute",
-                            ns=name,
-                            inputs={"trajectory": BlackboardKey("trajectory")},
-                            outputs={},
+                            target_pose_stamped_key=BlackboardKey("goal_pose"),
+                            tolerance_position=self.staging_configuration_tolerance_position,
+                            tolerance_orientation=(0.6, 0.5, 0.5),
+                            duration=10.0,
+                            round_decimals=3,
                         ),
                     ],
-                    post_behavior=get_toggle_collision_object_behavior(
-                        name + "DisallowWheelchairCollisionScopePost",
-                        [self.wheelchair_collision_object_id],
-                        False,
-                    ),
                 ),
             ],
         )
@@ -329,7 +262,18 @@ class MoveFromMouthTree(MoveToTree):
                         name + "AddInFrontOfWheelchairWallScopePre",
                         in_front_of_wheelchair_wall_id,
                     ),
+                    post_behavior=get_remove_in_front_of_wheelchair_wall_behavior(
+                        name + "RemoveInFrontOfWheelchairWallScopePost",
+                        in_front_of_wheelchair_wall_id,
+                    ),
                     workers=[
+                        # Retare the F/T sensor and set the F/T Thresholds
+                        pre_moveto_config(
+                            name=name + "PreMoveToConfigToEnd",
+                            toggle_watchdog_listener=False,
+                            f_mag=self.force_threshold_to_end_configuration,
+                            t_mag=self.torque_threshold_to_end_configuration,
+                        ),
                         # Goal configuration: staging configuration
                         MoveIt2JointConstraint(
                             name="EndingConfigurationGoalConstraint",
@@ -368,10 +312,6 @@ class MoveFromMouthTree(MoveToTree):
                             outputs={},
                         ),
                     ],
-                    post_behavior=get_remove_in_front_of_wheelchair_wall_behavior(
-                        name + "RemoveInFrontOfWheelchairWallScopePost",
-                        in_front_of_wheelchair_wall_id,
-                    ),
                 ),
             )
 
