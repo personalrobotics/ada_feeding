8a84d01ef8d29c65f0b6adc66d9a37dc87dd15a6
Ethan Gordon <ekgordon@cs.washington.edu>
2024-01-10 16:09:26 -0800

    [WIP] Added all linear policies

--- ada_feeding_action_select/ada_feeding_action_select/policies/linear_policies.py
+++ ada_feeding_action_select/ada_feeding_action_select/policies/linear_policies.py
@@ -82,7 +82,8 @@ class LinearPolicy(Policy):
         super().__init__(context_dim, posthoc_dim)
         self.library = get_action_library(library)
 
-        # Cache inverses in case a subclass needs them
+        # Cache inverses and other operations
+        # in case a subclass needs them
         self.cache = {}
 
         self.checkpoint = LinearPolicyCheckpoint(
@@ -135,6 +136,7 @@ class LinearPolicy(Policy):
         loss: float,
     ) -> Tuple[bool, str]:
         # Docstring copied from @override
+
         prob = action[0]
         if prob <= 0.0:
             return (False, "0 Probability in linear policy update")
@@ -160,10 +162,10 @@ class LinearPolicy(Policy):
         self.checkpoint.a_context_posthoc += posthoc_context.T
 
         # Update Policy
-        self._solve()
+        self._solve(arm=arm)
         return (True, "Success")
 
-    def _solve(self):
+    def _solve(self, arm=None):
         """
         Solve for policy from data matrices
         """
@@ -174,31 +176,39 @@ class LinearPolicy(Policy):
             self.checkpoint.lambda_l2 * np.eye(self.context_dim)
         )
         solve_b = self.checkpoint.b_context
+        solve_out = self.checkpoint.linear_model
 
         # Posthoc Augmented
         if self.checkpoint.use_posthoc:
             # Add Posthoc Dampening Term
-            a_posthoc = (
+            self.cache["a_posthoc_damp"] = (
                 self.checkpoint.a_posthoc
                 + self.checkpoint.lambda_posthoc_damp * np.eye(self.posthoc_dim)
             )
-            a_posthoc_inv = np.linalg.inv(np.sum(a_posthoc, axis=0))
+            self.cache["a_posthoc_inv"] = np.linalg.inv(
+                np.sum(self.cache["a_posthoc_damp"], axis=0)
+            )
             solve_a = (
                 solve_a
                 + self.checkpoint.a_context_posthoc
-                @ a_posthoc_inv
-                @ a_posthoc
-                @ a_posthoc_inv
+                @ self.cache["a_posthoc_inv"]
+                @ self.cache["a_posthoc_damp"]
+                @ self.cache["a_posthoc_inv"]
                 @ self.checkpoint.a_context_posthoc.T
             )
             solve_b = (
                 solve_b
                 + self.checkpoint.a_context_posthoc
-                @ a_posthoc_inv
+                @ self.cache["a_posthoc_inv"]
                 @ self.checkpoint.b_posthoc
             )
+        elif arm is not None:
+            solve_a = solve_a[arm, :]
+            solve_b = solve_b[arm, :]
+            solve_out = self.checkpoint.linear_model[arm, :]
+
         # Solve for linear policy vector
-        self.checkpoint.linear_model = np.linalge.solve(solve_a, solve_b)
+        solve_out[:] = np.linalge.solve(solve_a, solve_b)
 
     @override
     def get_checkpoint(self) -> Any:
@@ -253,4 +263,118 @@ class LinearPolicy(Policy):
         return True
 
 
-RandomPolicy = LinearPolicy
+RandomLinearPolicy = LinearPolicy
+
+
+class GreedyLinearPolicy(LinearPolicy):
+    """
+    Greedily takes the action
+    with the lowest expected loss.
+    """
+
+    @override
+    def choice(
+        self, context: npt.NDArray
+    ) -> Union[List[Tuple[float, AcquisitionSchema]], str]:
+        # Docstring copied from @override
+
+        ret_index = np.argmin(np.dot(context, self.checkpoint.linear_model.T))
+        return [(1.0, self.library[ret_index])]
+
+
+class EpsilonGreedyLinearPolicy(LinearPolicy):
+    """
+    With probability epsilon, take a random action.
+    With probability 1-epsilon, do the greedy policy.
+    """
+
+    def __init__(self, context_dim: int, posthoc_dim: int, epsilon: float, **kwargs):
+        """
+        Define self properties
+        """
+        super().__init__(context_dim, posthoc_dim, **kwargs)
+
+        if epsilon > 1.0 or epsilon < 0.0:
+            raise ValueError(
+                "Epsilon out of range [0.0, 1.0] in e-greedy linear policy."
+            )
+
+        self.epsilon = epsilon
+
+    @override
+    def choice(
+        self, context: npt.NDArray
+    ) -> Union[List[Tuple[float, AcquisitionSchema]], str]:
+        # Docstring copied from @override
+
+        best_index = np.argmin(np.dot(context, self.checkpoint.linear_model.T))
+        prob = np.ones(self.n_actions) * (self.epsilon / self.n_actions)
+        prob[best_index] += 1.0 - self.epsilon
+        assert np.isclose(np.sum(prob), 1.0)
+        return list(zip(prob, self.library))
+
+
+class LinUCBPolicy(LinearPolicy):
+    """
+    Take action with the loss
+    with the lowest lower-confidence-bound.
+    Called "UCB" for historical reasons (upper-bound on reward).
+    """
+
+    def __init__(self, context_dim: int, posthoc_dim: int, alpha: float, **kwargs):
+        """
+        Define self properties
+        """
+        super().__init__(context_dim, posthoc_dim, **kwargs)
+
+        self.alpha = alpha
+        self.covariance = (
+            np.array([np.eye(context_dim) for i in range(self.n_actions)])
+            / self.checkpoint.lambda_l2
+        )
+
+    @override
+    def choice(
+        self, context: npt.NDArray
+    ) -> Union[List[Tuple[float, AcquisitionSchema]], str]:
+        # Docstring copied from @override
+
+        lcb = self.checkpoint.linear_model @ context - (
+            self.alpha * np.sqrt(context.T @ self.covariance @ context)
+        )
+
+        return [(1.0, self.library[np.argmin(lcb)])]
+
+    @override
+    def update(
+        self,
+        posthoc: npt.NDArray,
+        context: npt.NDArray,
+        action: Tuple[float, AcquisitionSchema],
+        loss: float,
+    ) -> Tuple[bool, str]:
+        # Docstring copied from @override
+
+        # Run linear update
+        ret = super().update(posthoc, context, action, loss)
+        if not ret[0]:
+            return ret
+
+        # Calculate Covariance Matrix
+        cov_inv = self.checkpoint.a_context + (
+            np.eye(self.context_dim) * self.checkpoint.lambda_l2
+        )
+        if self.checkpoint.use_posthoc:
+            cov_inv += (
+                self.checkpoint.a_context_posthoc
+                @ self.cache["a_posthoc_inv"]
+                @ self.cache["a_posthoc_damp"]
+                @ self.cache["a_posthoc_inv"]
+                @ self.checkpoint.a_context_posthoc.T
+            )
+            self.covariance = 2.0 * np.linalg.inv(cov_inv)
+        else:
+            arm = self.library.index(action[1])
+            self.covariance[arm, :] = np.linalg.inv(cov_inv[arm, :])
+
+        return ret
