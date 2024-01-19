699be6c3ae481233c964e9e4f0fe5f615561be47
Ethan K. Gordon <ekgordon@cs.washington.edu>
2024-01-19 14:10:39 -0800

    Added color adapter

--- ada_feeding_action_select/ada_feeding_action_select/adapters/__init__.py
+++ ada_feeding_action_select/ada_feeding_action_select/adapters/__init__.py
@@ -14,5 +14,8 @@ from .base_adapters import NoContext
 # SPANet Context
 from .spanet_adapter import SPANetContext
 
+# Color Context
+from .color_adapter import ColorContext
+
 # HapticNet Posthoc
 from .hapticnet_adapter import HapticNetPosthoc
