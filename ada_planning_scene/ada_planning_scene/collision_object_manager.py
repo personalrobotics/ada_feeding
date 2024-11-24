5645921a9f799e2295760aca78677e1975ddd69b
Jose Jaime <jose33jaime@gmail.com>
2024-11-23 20:42:07 -0800

    Use Jaco+AF joint names and group name for MoveIt2 object in CollisionObjectManager

--- ada_planning_scene/ada_planning_scene/collision_object_manager.py
+++ ada_planning_scene/ada_planning_scene/collision_object_manager.py
@@ -49,10 +49,10 @@ class CollisionObjectManager:
         callback_group = ReentrantCallbackGroup()
         self.moveit2 = MoveIt2(
             node=self.__node,
-            joint_names=kinova.joint_names(),
+            joint_names=["j2n6s200_joint_1", "j2n6s200_joint_2", "j2n6s200_joint_3", "j2n6s200_joint_4", "j2n6s200_joint_5", "j2n6s200_joint_6", "af_joint_1", "af_joint_2"],
             base_link_name=kinova.base_link_name(),
             end_effector_name="forkTip",
-            group_name="jaco_arm",
+            group_name="jaco_af",
             callback_group=callback_group,
         )
 
