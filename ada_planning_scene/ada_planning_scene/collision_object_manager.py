7bd48d4176ebd2312903ca31aff14be242cbb330
Jose Jaime <jose33jaime@gmail.com>
2024-12-15 21:21:37 -0800

    Use ada move group for feeding

--- ada_planning_scene/ada_planning_scene/collision_object_manager.py
+++ ada_planning_scene/ada_planning_scene/collision_object_manager.py
@@ -52,7 +52,7 @@ class CollisionObjectManager:
             joint_names=kinova.joint_names(),
             base_link_name=kinova.base_link_name(),
             end_effector_name="forkTip",
-            group_name="jaco_arm",
+            group_name="ada",
             callback_group=callback_group,
         )
 
