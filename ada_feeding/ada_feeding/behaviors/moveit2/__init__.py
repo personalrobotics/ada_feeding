3c08e2b283cb673142bb03e5c97a9a16946e4733
Jose Jaime <jose33jaime@gmail.com>
2025-04-20 17:13:16 -0700

    Add MoveIt2ComputeFK behavior

--- ada_feeding/ada_feeding/behaviors/moveit2/__init__.py
+++ ada_feeding/ada_feeding/behaviors/moveit2/__init__.py
@@ -15,6 +15,7 @@ from .moveit2_constraints import (
     MoveIt2PoseConstraint,
 )
 from .moveit2_compute_ik import MoveIt2ComputeIK
+from .moveit2_compute_fk import MoveIt2ComputeFK
 from .servo_move import ServoMove
 
 # Modifying the planning scene
