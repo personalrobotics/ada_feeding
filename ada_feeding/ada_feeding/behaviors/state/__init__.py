96d6bb7454dcf44cf0d826da236ba166f2d21c58
Jose Jaime <jose33jaime@gmail.com>
2025-07-08 18:29:41 -0700

    Add behaviors for ExtractPoseFromTransformStamped and OffsetPositionFromPose

--- ada_feeding/ada_feeding/behaviors/state/__init__.py
+++ ada_feeding/ada_feeding/behaviors/state/__init__.py
@@ -32,3 +32,9 @@ from .load_pinocchio_model import (
 from .check_articutool_path_leveling_feasibility import (
     CheckArticutoolPathLevelingFeasibility,
 )
+from .offset_position_from_pose import (
+    OffsetPositionFromPose,
+)
+from .extract_pose_from_transform_stamped import (
+    ExtractPoseFromTransformStamped,
+)
