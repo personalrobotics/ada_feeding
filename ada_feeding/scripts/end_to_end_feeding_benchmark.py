13c6d499d52db42c0d36884fe1e312d09aa65acd
Jose Jaime <jose33jaime@gmail.com>
2025-08-27 08:14:55 -0700

    Tighten position tolerance for AbovePlate stage

--- ada_feeding/scripts/end_to_end_feeding_benchmark.py
+++ ada_feeding/scripts/end_to_end_feeding_benchmark.py
@@ -2149,9 +2149,7 @@ class EndToEndBenchmark:
     ) -> Tuple[TrialStatus, Optional[JointTrajectory], float]:
         LOGGER.info("  Planning to AbovePlate pose...")
         goal_constraints = [
-            create_pose_constraint(
-                above_plate_pose, tolerance_position=0.2, tolerance_orientation=0.2
-            )
+            create_pose_constraint(above_plate_pose, tolerance_orientation=0.2)
         ]
 
         return self.motion_planner.plan(
