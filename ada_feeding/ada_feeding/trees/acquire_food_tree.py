532c45c4f7914596f2d2e97c99521a0728e9e1e7
Jose Jaime <jose33jaime@gmail.com>
2025-09-13 13:46:38 -0700

    Restore wheelchair wall spawning

--- ada_feeding/ada_feeding/trees/acquire_food_tree.py
+++ ada_feeding/ada_feeding/trees/acquire_food_tree.py
@@ -201,19 +201,19 @@ class AcquireFoodTree(MoveToTree):
                 scoped_behavior(
                     name=name + " InFrontOfWheelchairWallScope",
                     # TODO: Revert this when not running benchmarks
-                    pre_behavior=(
-                        Success()  # pylint: disable=abstract-class-instantiated
-                    ),
-                    post_behavior=(
-                        Success()  # pylint: disable=abstract-class-instantiated
-                    ),
-                    # pre_behavior=get_add_in_front_of_face_wall_behavior(
-                    #     name + "AddWheelchairWall",
+                    # pre_behavior=(
+                    #     Success()  # pylint: disable=abstract-class-instantiated
                     # ),
-                    # # Remove the wall in front of the wheelchair
-                    # post_behavior=get_remove_in_front_of_face_wall_behavior(
-                    #     name + "RemoveWheelchairWall",
+                    # post_behavior=(
+                    #     Success()  # pylint: disable=abstract-class-instantiated
                     # ),
+                    pre_behavior=get_add_in_front_of_face_wall_behavior(
+                        name + "AddWheelchairWall",
+                    ),
+                    # Remove the wall in front of the wheelchair
+                    post_behavior=get_remove_in_front_of_face_wall_behavior(
+                        name + "RemoveWheelchairWall",
+                    ),
                     workers=[
                         py_trees_ros.service_clients.FromConstant(
                             name="ClearOctomap",
