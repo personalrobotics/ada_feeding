90d7065ed11dbd1e5924c4a430e05e50b5dd93ac
Jose Jaime <jose33jaime@gmail.com>
2025-11-09 11:10:27 -0800

    Remove comment

--- ada_feeding/ada_feeding/trees/move_to_configuration_with_wheelchair_wall_tree.py
+++ ada_feeding/ada_feeding/trees/move_to_configuration_with_wheelchair_wall_tree.py
@@ -347,13 +347,6 @@ class MoveToConfigurationWithWheelchairWallTree(MoveToTree):
                 # from moving unnecessarily close to the user.
                 scoped_behavior(
                     name=name + " InFrontOfWheelchairWallScope",
-                    # TODO: Revert this when not running benchmarks
-                    # pre_behavior=(
-                    #     Success()  # pylint: disable=abstract-class-instantiated
-                    # ),
-                    # post_behavior=(
-                    #     Success()  # pylint: disable=abstract-class-instantiated
-                    # ),
                     pre_behavior=get_add_in_front_of_face_wall_behavior(
                         name + "AddWheelchairWall",
                     ),
